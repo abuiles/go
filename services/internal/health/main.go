3045a4a5ad1ff32f0531b5a991d09e27ca749f3c
Adolfo Builes <builes.adolfo@gmail.com>
2020-03-03 18:33:17 -0500

    Move things around a little bit.

--- services/internal/health/main.go
+++ services/internal/health/main.go
@@ -26,6 +26,30 @@ type Config struct {
 	Port *int `valid:"required"`
 }
 
+// Cron interface
+type Cron interface {
+	AddJob(spec string, cmd cron.Job) error
+	Entries() []*cron.Entry
+	Start()
+}
+
+// App is the application object
+type App struct {
+	config  Config
+	cron    Cron
+	metrics metrics.Registry
+}
+
+// NewApp constructs an new App instance from the provided config.
+func NewApp(config Config) (app *App, err error) {
+	app = &App{
+		config:  config,
+		cron:    cron.New(),
+		metrics: metrics.NewRegistry(),
+	}
+	return
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	rootCmd.Execute()
@@ -52,68 +76,88 @@ func run(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	// TODO: Stop cron after when shuttingdown the server
-	app.StartCron()
+	// TODO: Stop crons when shuttingdown the server gracefully
+	app.StartCrons()
 	app.Serve()
 }
 
-// Cron interface
-type Cron interface {
-	AddJob(spec string, cmd cron.Job) error
-	Entries() []*cron.Entry
-	Start()
+// Serve starts the server
+func (a *App) Serve() {
+	// TODO: maybe we don't need all of this and we can go with a simple http.Server
+	mux := supportHttp.NewAPIMux(supportLog.DefaultLogger)
+
+	// Middlewares
+	headers := make(http.Header)
+	headers.Set("Content-Type", "application/json")
+
+	mux.Method(http.MethodGet, "/", IndexHandler{app: a})
+	mux.Method(http.MethodGet, "/metrics", MetricsHandler{app: a})
+
+	supportHttp.Run(supportHttp.Config{
+		ListenAddr: fmt.Sprintf(":%d", *a.config.Port),
+		Handler:    mux,
+		OnStarting: func() {
+			log.Infof("starting health-check server")
+			log.Infof("listening on %d", *a.config.Port)
+		},
+	})
 }
 
-// App is the application object
-type App struct {
-	config  Config
-	cron    Cron
-	metrics metrics.Registry
-}
+// ----------------------------------------------------------------------------------------------
+// Commands infra
+//
 
-// NewApp constructs an new App instance from the provided config.
-func NewApp(config Config) (app *App, err error) {
-	app = &App{
-		config:  config,
-		cron:    cron.New(),
-		metrics: metrics.NewRegistry(),
-	}
-	return
-}
-
-// HorizonCmp wrapper for HorizonCmp tool
-type HorizonCmp struct {
+// Cmd wrapper for Cmd tool
+type Cmd struct {
 	Name   string `json:"name"`
 	metric metrics.Gauge
+	base   string // <--- this is used just for testing, we should pass the command as an arg
 }
 
-func (cmp *HorizonCmp) Run() {
-	// TODO maybe add a mutex and avoid running if there is a command currently running
-
-	cmd := exec.Command("horizon-cmp", "history", "-t", "https://horizon.stellar.org", "-b", "https://horizon-testnet.stellar.org", "--count", "4")
+// Run runs command
+func (c *Cmd) Run() {
+	log.Infof("Running %s", c.Name)
+	// TODO add a mutex and avoid running if there is a command currently running
+	// TODO make  command configurable
+	cmd := exec.Command("horizon-cmp", "history", "-t", "https://horizon.stellar.org", "-b", c.base, "--count", "4")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
 		log.Errorf("cmd.Run() failed with %s\n", err)
-		cmp.metric.Update(1)
+		c.metric.Update(1)
 		return
 	}
-	cmp.metric.Update(0)
+	c.metric.Update(0)
 }
 
-// StartCron starts cron job
-func (a *App) StartCron() {
-	horizonCmp := HorizonCmp{
+// StartCrons starts cron job
+func (a *App) StartCrons() {
+	// TODO: read each command from a config file, metric type and frequency
+	horizonCmp := Cmd{
 		Name:   "HorizonCmp",
 		metric: metrics.NewGauge(),
+		base:   "https://horizon-testnet.stellar.org",
 	}
 	a.cron.AddJob("@every 20s", &horizonCmp)
-	a.metrics.Register("horizon_cmp.failures", horizonCmp.metric)
+	a.metrics.Register("horizon_cmp_testnet.failures", horizonCmp.metric)
+
+	horizonCmpMain := Cmd{
+		Name:   "HorizonCmpMain",
+		metric: metrics.NewGauge(),
+		base:   "https://horizon-blue.stellar.org",
+	}
+	a.cron.AddJob("@every 30s", &horizonCmpMain)
+	a.metrics.Register("horizon_cmp_pubnet.failures", horizonCmpMain.metric)
 
 	a.cron.Start()
 }
 
+// ----------------------------------------------------------------------------------------------
+// HTTP Handlers
+//
+
+// MetricsHandler http handler for /metrics
 type MetricsHandler struct {
 	// we should probably pass this through ctx
 	app *App
@@ -132,38 +176,17 @@ func (handler MetricsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 	})
 }
 
-type CronHandler struct {
+// IndexHandler handler for /
+type IndexHandler struct {
 	// we should probably pass this through ctx?
 	app *App
 }
 
-func (handler CronHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (handler IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	body, err := json.Marshal(handler.app.cron.Entries())
 	if err != nil {
 		w.Write([]byte(`{"error": "error reading cron job entries"}`))
 	}
 
 	w.Write(body)
-}
-
-// Serve starts the server
-func (a *App) Serve() {
-	// TODO: maybe we don't need all of this and we can go with a simple http.Server
-	mux := supportHttp.NewAPIMux(supportLog.DefaultLogger)
-
-	// Middlewares
-	headers := make(http.Header)
-	headers.Set("Content-Type", "application/json")
-
-	mux.Method(http.MethodGet, "/", CronHandler{app: a})
-	mux.Method(http.MethodGet, "/metrics", MetricsHandler{app: a})
-
-	supportHttp.Run(supportHttp.Config{
-		ListenAddr: fmt.Sprintf(":%d", *a.config.Port),
-		Handler:    mux,
-		OnStarting: func() {
-			log.Infof("starting health-check server")
-			log.Infof("listening on %d", *a.config.Port)
-		},
-	})
 }
